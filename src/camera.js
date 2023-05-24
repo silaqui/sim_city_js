779f09f50c49a6fd0ae2b4c53756cff8a33907b5
Miki <aivi.pol@gmail.com>
2023-05-24 20:43:03 +0200

    Refactor camera consts

--- src/camera.js
+++ src/camera.js
@@ -9,16 +9,19 @@ export function createCamera(gameWindow) {
 
     const MIN_CAMERA_RADIUS = 2;
     const MAX_CAMERA_RADIUS = 10;
+    const MIN_CAMERA_ELEVATION = 30;
+    const MAX_CAMERA_ELEVATION = 90;
+    const ROTATION_SENSITIVITY = 0.5;
+    const ZOOM_SENSITIVITY = 0.02;
+    const PAN_SENSITIVITY = -0.01;
 
     const Y_AXIS = new THREE.Vector3(0, 1, 0);
-    const X_AXIS = new THREE.Vector3(1, 0, 0);
-    const Z_AXIS = new THREE.Vector3(0, 0, 1);
 
     const camera = new THREE.PerspectiveCamera(75, gameWindow.offsetWidth / gameWindow.offsetHeight, 0.1, 1000);
     let cameraOrigin = new THREE.Vector3()
     let cameraRadius = 4;
     let cameraAzimuth = 0;
-    let cameraElevation = 0;
+    let cameraElevation = 30;
     let isLeftMouseDown = false;
     let isRightMouseDown = false;
     let isMiddleMouseDown = false;
@@ -44,22 +47,22 @@ export function createCamera(gameWindow) {
         let deltaY = event.clientY - prevMouseY;
 
         if (isLeftMouseDown) {
-            cameraAzimuth += -(deltaX * 0.5);
-            cameraElevation += (deltaY * 0.5);
-            cameraElevation = Math.min(90, Math.max(0, cameraElevation));
+            cameraAzimuth += -(deltaX * ROTATION_SENSITIVITY);
+            cameraElevation += (deltaY * ROTATION_SENSITIVITY);
+            cameraElevation = Math.min(MAX_CAMERA_ELEVATION, Math.max(MIN_CAMERA_ELEVATION, cameraElevation));
             updateCameraPosition();
         }
 
         if (isMiddleMouseDown) {
             const forward = new THREE.Vector3(0, 0, 1).applyAxisAngle(Y_AXIS, cameraAzimuth * DEG2RAD)
             const left = new THREE.Vector3(1, 0, 0).applyAxisAngle(Y_AXIS, cameraAzimuth * DEG2RAD)
-            cameraOrigin.add(forward.multiplyScalar(-0.01 * deltaY));
-            cameraOrigin.add(left.multiplyScalar(-0.01 * deltaX));
+            cameraOrigin.add(forward.multiplyScalar(PAN_SENSITIVITY * deltaY));
+            cameraOrigin.add(left.multiplyScalar(PAN_SENSITIVITY * deltaX));
             updateCameraPosition();
         }
 
         if (isRightMouseDown) {
-            cameraRadius += deltaY * 0.02;
+            cameraRadius += deltaY * ZOOM_SENSITIVITY;
             cameraRadius = Math.min(MAX_CAMERA_RADIUS, Math.max(MIN_CAMERA_RADIUS, cameraRadius));
             updateCameraPosition();
         }
