040cb80b51de7ff18912ee814bd202466c55a96a
Miki <aivi.pol@gmail.com>
2023-05-24 15:35:58 +0200

    Import from a CDN

--- src/scene.js
+++ src/scene.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://unpkg.com/three/build/three.module.js";
+import * as THREE from 'three';
 
 export function createScene() {
     const gameWindow = document.getElementById('render-target')
